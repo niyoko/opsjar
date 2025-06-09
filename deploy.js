5e4865163315acf5520e55e466361e35b46d3e44
Niyoko Yuliawan <niyoko.yuliawan@logisly.com>
2025-06-09 10:48:57 +0700

    update

--- deploy.js
+++ deploy.js
@@ -6,7 +6,14 @@ async function deployMyCode() {
     server: "ftp.opsjar.site",
     username: "git@opsjar.site",
     password: `ZkW34fMg2fKEbf6cwDDPt7pr`,
-    exclude: [...excludeDefaults, "_protected/backend/web/assets/**", "assets/**"],
+    exclude: [
+      ...excludeDefaults,
+      "_protected/common/config/main-local.php",
+      "_protected/backend/config/main-local.php",
+      "_protected/backend/web/assets/**",
+      "assets/**",
+      "deploy.js",
+    ],
   });
   console.log("🚀 Deploy done!");
 }
